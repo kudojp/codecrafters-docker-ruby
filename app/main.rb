cf0b3c7d9c1b131fa91b4c57abfe24debe8b3a5b
kudojp <heyjudejudejude1968@gmail.com>
2023-06-10 17:27:50 +0900

    Stage5: Use unshare command to run command in a new pid namespace

--- app/main.rb
+++ app/main.rb
@@ -4,6 +4,7 @@ require "fileutils"
 docker_command = ARGV[0]
 image_tag = ARGV[1]
 command_line = ARGV[2...]
+# command_line[0] is assumed to be `/usr/local/bin/docker-explorer`
 
 if docker_command != 'run'
   $stderr.puts "Currently only 'mydocker run' is supported"
@@ -13,7 +14,11 @@ end
 FileUtils.mkdir_p("/app/root_dir/usr/local/bin")
 FileUtils.cp("/usr/local/bin/docker-explorer", "/app/root_dir/usr/local/bin/docker-explorer")
 
-stdout, stderr, status = Open3.capture3("chroot", "/app/root_dir", *command_line)
+# ref. https://blog.amedama.jp/entry/linux-pid-namespace_1
+unshare_command_line = ["unshare", "--pid", "--fork", "--mount-proc"]
+chmod_command_line = ["chroot", "/app/root_dir"]
+
+stdout, stderr, status = Open3.capture3(*unshare_command_line, *chmod_command_line, *command_line)
 $stdout.puts stdout
 $stderr.puts stderr
 exit status.exitstatus
