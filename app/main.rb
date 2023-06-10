049c3f8a67c630e6217f8761a008040f792acdbf
kudojp <heyjudejudejude1968@gmail.com>
2023-06-10 15:33:37 +0900

    Stage4: Use docker-explorer copied within the ruby:2.7-alpine Docker container

--- app/main.rb
+++ app/main.rb
@@ -1,16 +1,19 @@
 require "open3"
+require "fileutils"
 
 docker_command = ARGV[0]
 image_tag = ARGV[1]
-command = ARGV[2]
-command_args = ARGV[3...]
+command_line = ARGV[2...]
 
 if docker_command != 'run'
   $stderr.puts "Currently only 'mydocker run' is supported"
   exit 1
 end
 
-stdout, stderr, status = Open3.capture3("chroot", "./root_dir", command, *command_args)
+FileUtils.mkdir_p("/app/root_dir/usr/local/bin")
+FileUtils.cp("/usr/local/bin/docker-explorer", "/app/root_dir/usr/local/bin/docker-explorer")
+
+stdout, stderr, status = Open3.capture3("chroot", "/app/root_dir", *command_line)
 $stdout.puts stdout
 $stderr.puts stderr
 exit status.exitstatus
