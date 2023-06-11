f541d957eb1bc966cd215d598197ea1953c1c4f6
kudojp <heyjudejudejude1968@gmail.com>
2023-06-11 18:31:10 +0900

    Rename and update BaseImageReproducer, which works with ubuntu, but not with alpine/busybox

--- app/base_image_reproducer.rb
+++ app/base_image_reproducer.rb
@@ -1,29 +1,32 @@
 require 'json'
 require 'net/http'
 
-class ImageFetcher
+class BaseImageReproducer
   REGISTRY_HOST = "registry.hub.docker.com".freeze
   OFFICIAL_LIBRARY_REPOS = ["ubuntu", "alpine", "busybox"].freeze
 
-  def initialize(repo, tag)
+  # root_dir (path in the host machine) will be the root directory of the running container.
+  def initialize(repo, tag, root_dir)
     repo = "library/#{repo}" if OFFICIAL_LIBRARY_REPOS.include? repo
 
     @repo = repo
     @tag = tag
+    @root_dir = root_dir
   end
 
-  def layers
-    amd_manifest_digest = self.manifest_list[0]["digest"] # arch = amd # TODO: fix with filtering
-    self.image_layers(amd_manifest_digest).each_with_object([]) do |layer, layers|
-      layers << self.image_layer(layer["digest"])
+  def reproduce!
+    # TODO
+    amd_manifest_digest = self.manifest_digests_list[0]["digest"] # Assuming that the host machine uses "amd" architecture
+    self.layer_digests_list(amd_manifest_digest).each do |layer_digest|
+      reproduce_layer!(layer_digest["digest"])
     end
   end
 
   private
 
-  # Return value is formatted like:
+  # Return value is the list of digests and is formatted like:
   # [{"digest":"sha256:2fdb1cf4995abb74c035e5f520c0f3a46f12b3377a59e86ecca66d8606ad64f9","mediaType":"application/vnd.oci.image.manifest.v1+json","platform":{"architecture":"amd64","os":"linux"},"size":424},{"digest":"sha256:c80ed91cdc47229010c4f34f96c3442bc02dca260d0bf26f6c4b047ea7d11cf2","mediaType":"application/vnd.oci.image.manifest.v1+json","platform":{"architecture":"arm","os":"linux","variant":"v7"},"size":424},{"digest":"sha256:77bdd217935d10f0e753ed84118e9b11d3ab0a66a82bdf322087354ccd833733","mediaType":"application/vnd.oci.image.manifest.v1+json","platform":{"architecture":"arm64","os":"linux","variant":"v8"},"size":424},{"digest":"sha256:268686ba2c6284461cae1642d9d055e51b16f8e711d49b34638146b78050f5a0","mediaType":"application/vnd.oci.image.manifest.v1+json","platform":{"architecture":"ppc64le","os":"linux"},"size":424},{"digest":"sha256:b0b966f885ea29d809d03d027c3d21182676380b241c3a271aa83f8e9d7bac06","mediaType":"application/vnd.oci.image.manifest.v1+json","platform":{"architecture":"s390x","os":"linux"},"size":424}]
-  def manifest_list()
+  def manifest_digests_list()
     # ref. https://docs.docker.com/registry/spec/api/#pulling-an-image-manifest
     url = URI("https://#{REGISTRY_HOST}/v2/#{@repo}/manifests/#{@tag}")
     token = get_bearer_token(url)
@@ -35,12 +38,12 @@ class ImageFetcher
       http.request req
     end
 
-    JSON.load(response.body)["manifests"]
+    JSON.load(response.body).fetch("manifests")
   end
 
   # Return value is formatted like:
   # [{"mediaType"=>"application/vnd.oci.image.layer.v1.tar+gzip", "size"=>29534702, "digest"=>"sha256:837dd4791cdc6f670708c3a570b72169263806d7ccc2783173b9e88f94878271"}]
-  def image_layers(manifest_digest)
+  def layer_digests_list(manifest_digest)
     # ref. https://docs.docker.com/registry/spec/api/#pulling-an-image-manifest
     url = URI("https://#{REGISTRY_HOST}/v2/#{@repo}/manifests/#{manifest_digest}")
     token = get_bearer_token(url)
@@ -52,11 +55,10 @@ class ImageFetcher
       http.request req
     end
 
-    puts puts response.body
-    JSON.load(response.body)["layers"]
+    JSON.load(response.body).fetch("layers")
   end
 
-  def image_layer(layer_digest)
+  def reproduce_layer!(layer_digest)
     url = URI("https://#{REGISTRY_HOST}/v2/#{@repo}/blobs/#{layer_digest}")
     token = get_bearer_token(url)
 
@@ -72,8 +74,15 @@ class ImageFetcher
       response = self.redirect_for_image_layer(response['location'], response['location'])
     end
 
-    puts response.header.to_hash
-    response.body
+    base_layer_file = "base_layer.tar.gz"
+
+    # tar_file_place = "#{@root_dir}/base_layer.tar.gz"
+
+    File.open("#{@root_dir}/#{base_layer_file}", "w") do |f|
+      f.write(response.body)
+    end
+
+    stdout, stderr, status = Open3.capture3("tar xvzf #{@root_dir}/#{base_layer_file} -C #{@root_dir}")
   end
 
   def redirect_for_image_layer(url, redirect_limit=5)
--- app/main.rb
+++ app/main.rb
@@ -1,17 +1,25 @@
 require "open3"
 require "fileutils"
+require "./app/base_image_reproducer"
 
 docker_command = ARGV[0]
 image_tag = ARGV[1]
 command_line = ARGV[2...]
 # command_line[0] is assumed to be `/usr/local/bin/docker-explorer`
 
+repo, tag = image_tag.split(":", 2)
+tag ||= "latest"
+
+root_dir = "/app/root_dir"
+
+FileUtils.mkdir_p(root_dir) # This is used as the root directory of the container
+BaseImageReproducer.new(repo, tag, root_dir).reproduce!
+
 if docker_command != 'run'
   $stderr.puts "Currently only 'mydocker run' is supported"
   exit 1
 end
 
-FileUtils.mkdir_p("/app/root_dir") # This is used as the root directory of the container
 
 FileUtils.mkdir_p("/app/root_dir/usr/local/bin")
 FileUtils.cp("/usr/local/bin/docker-explorer", "/app/root_dir/usr/local/bin/docker-explorer")
